c9fe7fa61ec2fbd13102583d06f80137ce1028b5
spietrek <spietrek@gmail.com>
2022-06-13 09:56:38 -0400

    add icon and text

--- src/components/molecules/NbosCard.jsx
+++ src/components/molecules/NbosCard.jsx
@@ -1,10 +1,16 @@
 // import PropTypes from 'prop-types'
+import { NbosIcon } from '../atoms/NbosIcon'
+import { NbosText } from '../atoms/NbosText'
 
 export const NbosCard = () => {
   return (
-    <div>
-      <span>NbosCard Component</span>
-    </div>
+    <>
+      <div>
+        <span>NbosCard Component</span>
+      </div>
+      <NbosIcon />
+      <NbosText />
+    </>
   )
 }
 
