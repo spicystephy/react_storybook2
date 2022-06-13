e628c6d68d713a1b17f66ffc31e776391ed17be0
spietrek <spietrek@gmail.com>
2022-06-13 07:25:55 -0400

    add material ui icons and test data

---
+++ src/stories/data/testData-cards.js
@@ -0,0 +1,32 @@
+const cards = [
+  {
+    key: 1,
+    title: 'Total Projects',
+    count: 107,
+    color: '#9900cc',
+    icon: 'auto_awesome_motion',
+  },
+  {
+    key: 2,
+    title: 'Red Projects',
+    count: 18,
+    color: '#ff0000',
+    icon: 'error',
+  },
+  {
+    key: 3,
+    title: 'Yellow Projects',
+    count: 48,
+    color: '#e6b800',
+    icon: 'warning',
+  },
+  {
+    key: 4,
+    title: 'Green Projects',
+    count: 41,
+    color: '#009933',
+    icon: 'thumb_up',
+  },
+]
+
+export default cards
