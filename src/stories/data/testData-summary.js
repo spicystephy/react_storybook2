b14cd66a0681c3f588d25f8e512a0afc71c34f3b
Stephanie <stephaniemzavala@gmail.com>
2022-06-29 08:45:11 -0700

    update to reflect design doc

--- src/stories/data/testData-summary.js
+++ src/stories/data/testData-summary.js
@@ -1,11 +1,11 @@
 export const summary1 = {
-  ttm_avg_loan_balance: '$519.09k',
-  ttm_avg_credit_commitments: '$549.13k',
+  ttm_avg_loan_balance: '$519.09 K',
+  ttm_avg_credit_commitments: '$549.13 K',
   ttm_deposit_balance: '$0.00',
 }
 
 export const summary2 = {
-  ttm_revenue: '$518.89k',
+  ttm_revenue: '$518.89 K',
   ttm_avg_raroc: '4.41%',
   fee_equivalent_card_percent_load_commit: '0.94%',
 }
