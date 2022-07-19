14e4e3821d8aa660089025a188fe8dd24a953367
Stephanie <stephaniemzavala@gmail.com>
2022-07-19 08:34:59 -0700

    fixes to style

--- src/components/features/dashboard/NbosDashboardTemplate.jsx
+++ src/components/features/dashboard/NbosDashboardTemplate.jsx
@@ -2,14 +2,16 @@ import { NbosProfile } from '../../organisms/NbosProfile'
 import { NbosClientOverview } from '../../organisms/NbosClientOverview'
 import { NbosSummary1 } from '../../organisms/NbosSummary1'
 import { NbosSummary2 } from '../../organisms/NbosSummary2'
+// import { NbosHighchartsColumn } from 'components/molecules/NbosHighchartsColumn'
+// import { NbosHighchartsBar } from 'components/molecules/NbosHighchartsBar'
 
 export const NbosDashboardTemplate = ({ user, client, summary1, summary2 }) => {
   return (
-    <div className="tw-grid tw-grid-cols-2 tw-gap-6">
-      <div>
+    <div className="tw-grid tw-grid-cols-3 tw-gap-6 tw-display-inline-block">
+      <div className="tw-col-span-1">
         <NbosProfile user={user} />
       </div>
-      <div className="tw-grid tw-gap-8">
+      <div className="tw-grid tw-col-span-2 tw-gap-8">
         <NbosClientOverview client={client} />
         <NbosSummary1 summary1={summary1} />
         <NbosSummary2 summary2={summary2} />
