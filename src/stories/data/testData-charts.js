5ed4293d4d04520bfcf97852948bc5e6d5f23633
Stephanie <stephaniemzavala@gmail.com>
2022-07-20 07:55:02 -0700

    added data tables

--- src/stories/data/testData-charts.js
+++ src/stories/data/testData-charts.js
@@ -1,60 +1,66 @@
-export const outcomeMetrics = {
-  loanProduction: {
-    year1: 1,
-    year2: 2,
+export const outcomeMetrics = [
+  {
+    loanProduction: {
+      year1: 1,
+      year2: 2,
+    },
+    depositGrowth: {
+      year1: 3,
+      year2: 4,
+    },
+    tmGrowth: {
+      year1: 5,
+      year2: 6,
+    },
+    newClients: {
+      year1: 7,
+      year2: 8,
+    },
   },
-  depositGrowth: {
-    year1: 3,
-    year2: 4,
-  },
-  tmGrowth: {
-    year1: 5,
-    year2: 6,
-  },
-  newClients: {
-    year1: 7,
-    year2: 8,
-  },
-}
+]
 
-export const behaviorMetrics = {
-  satisfaction: {
-    year1: 1,
-    year2: 2,
+export const behaviorMetrics = [
+  {
+    satisfaction: {
+      year1: 1,
+      year2: 2,
+    },
+    clientCalls: {
+      year1: 3,
+      year2: 4,
+    },
+    prospectCalls: {
+      year1: 5,
+      year2: 6,
+    },
+    stratsUpdated: {
+      year1: 7,
+      year2: 8,
+    },
   },
-  clientCalls: {
-    year1: 3,
-    year2: 4,
-  },
-  prospectCalls: {
-    year1: 5,
-    year2: 6,
-  },
-  stratsUpdated: {
-    year1: 7,
-    year2: 8,
-  },
-}
+]
 
-export const opportunitiesSummary = {
-  stage1: {
-    year1: 1,
-    year2: 2,
+export const opportunitiesSummary = [
+  {
+    stage1: {
+      year1: 1,
+      year2: 2,
+    },
+    stage2: {
+      year1: 3,
+      year2: 4,
+    },
+    stage3: {
+      year1: 5,
+      year2: 6,
+    },
+    stage4: {
+      year1: 7,
+      year2: 8,
+    },
+    bookedYtd: {
+      year1: 9,
+      year2: 10,
+    },
   },
-  stage2: {
-    year1: 3,
-    year2: 4,
-  },
-  stage3: {
-    year1: 5,
-    year2: 6,
-  },
-  stage4: {
-    year1: 7,
-    year2: 8,
-  },
-  bookedYtd: {
-    year1: 9,
-    year2: 10,
-  },
-}
+]
