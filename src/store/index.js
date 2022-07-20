bb5043b88fffcdedd07340d4d39902d81e6128b4
Stephanie <stephaniemzavala@gmail.com>
2022-07-20 07:55:34 -0700

    added slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../components/features/counter/counterSlice'
 import todosReducer from './todosSlice'
+import profileReducer from './profileSlice'
+import clientReducer from './clientSlice'
+import summary1Reducer from './summary1Slice'
+import summary2Reducer from './summary2Slice'
 
 const reducers = {
   counter: counterReducer,
   todos: todosReducer,
+  profile: profileReducer,
+  client: clientReducer,
+  summary1: summary1Reducer,
+  summary2: summary2Reducer,
 }
 
 export const store = configureStore({
