70c18c8218bbb2b4e84b43444508d43ef9142bed
Stephanie <stephaniemzavala@gmail.com>
2022-07-27 07:37:32 -0700

    added metrics card

--- src/components/features/dashboard/NbosDashboardTemplate.jsx
+++ src/components/features/dashboard/NbosDashboardTemplate.jsx
@@ -2,10 +2,14 @@ import { NbosProfile } from '../../organisms/NbosProfile'
 import { NbosClientOverview } from '../../organisms/NbosClientOverview'
 import { NbosSummary1 } from '../../organisms/NbosSummary1'
 import { NbosSummary2 } from '../../organisms/NbosSummary2'
-// import { NbosHighchartsColumn } from 'components/molecules/NbosHighchartsColumn'
-// import { NbosHighchartsBar } from 'components/molecules/NbosHighchartsBar'
-
-export const NbosDashboardTemplate = ({ user, client, summary1, summary2 }) => {
+import { NbosMetrics } from 'components/organisms/NbosMetrics'
+export const NbosDashboardTemplate = ({
+  user,
+  client,
+  summary1,
+  summary2,
+  metrics,
+}) => {
   return (
     <div className="tw-grid tw-grid-cols-3 tw-gap-6 tw-display-inline-block">
       <div className="tw-col-span-1">
@@ -15,6 +19,9 @@ export const NbosDashboardTemplate = ({ user, client, summary1, summary2 }) => {
         <NbosClientOverview client={client} />
         <NbosSummary1 summary1={summary1} />
         <NbosSummary2 summary2={summary2} />
+      </div>
+      <div className="tw-col-span-3">
+        <NbosMetrics metrics={metrics} />
       </div>
     </div>
   )
