421d6e90af67f33ca2af434ef3b8a1eaab6f3ef0
Stephanie <stephaniemzavala@gmail.com>
2022-08-08 15:43:35 -0700

    metrics added

--- src/components/pages/dashboard/index.jsx
+++ src/components/pages/dashboard/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
@@ -6,26 +7,44 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { NbosDashboardTemplate } from '../../features/dashboard/NbosDashboardTemplate'
+import { NbosMetrics } from 'components/organisms/NbosMetrics'
+import { NbosOpportunitiesTable } from 'components/organisms/NbosOpportunitiesTable'
+import { NbosHighchartsColumn } from 'components/molecules/NbosHighchartsColumn'
 import { useDispatch, useSelector } from 'react-redux'
-import { user } from '../../../stories/data/testData-user'
-import { client } from '../../../stories/data/testData-client'
-import { summary1 } from '../../../stories/data/testData-summary1'
-import { summary2 } from '../../../stories/data/testData-summary2'
+// import { user } from '../../../stories/data/testData-user'
+// import { client } from '../../../stories/data/testData-client'
+// import { summary1 } from '../../../stories/data/testData-summary1'
+// import { summary2 } from '../../../stories/data/testData-summary2'
 import { store } from 'store'
-import { getClientData } from 'store/clientsSlice'
+import { getClientsData } from 'store/clientsSlice'
 import { getUsers } from 'store/usersSlice'
 import { getSummary1 } from 'store/summary1Slice'
 import { getSummary2 } from 'store/summary2Slice'
+import { getOutcomeMetrics } from 'store/outcomeSlice'
+import { getBehaviorMetrics } from 'store/behaviorSlice'
+import { NbosSummary1 } from 'components/organisms/NbosSummary1'
 
 export function DashboardPage() {
   const userProfile = useSelector(state => state.userProfile)
-  const clientData = useSelector(state => state.clientData)
+  const clientsData = useSelector(state => state.clientData)
   const summary1 = useSelector(state => state.summary1)
   const summary2 = useSelector(state => state.summary2)
+  const outcomeMetrics = useSelector(state => state.outcomeMetrics)
+  const behaviorMetrics = useSelector(state => state.behaviorMetrics)
   const dispatch = useDispatch()
+
+  useEffect(async () => {
+    await dispatch(getUsers())
+    await dispatch(getClientsData())
+    await dispatch(getSummary1())
+    await dispatch(getSummary2())
+    await dispatch(getOutcomeMetrics())
+    await dispatch(getBehaviorMetrics())
+  }, [])
+
   return (
     <>
-      <Box sx={{ flexGrow: 1, user }}>
+      <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
             <IconButton
@@ -50,12 +69,15 @@ export function DashboardPage() {
             <div className="tw-bg-slate-200 tw-h-screen" />
           </aside>
           <section className="tw-grow">
-            <NbosDashboardTemplate
+            {/* <NbosDashboardTemplate
               user={user}
               client={client}
               summary1={summary1}
               summary2={summary2}
-            />
+            /> */}
+            <NbosMetrics />
+            <NbosHighchartsColumn />
+            <NbosOpportunitiesTable />
           </section>
         </div>
       </main>
